a00191582ed6d31bd2caa78fe747b323ee010d5c
Jordi <wuyingren@users.noreply.github.com>
2014-08-08 19:48:25 +0200

    Delete consent.js
    File consent.js was a test, accidentally commited

--- js/consent.js
+++
@@ -1,8 +0,0 @@
- $(document).on('pagechange', function() {
-            if(localStorage.getItem("user_uuid")==null) {
-                console.log("consenting");
-                setTimeout(function () {
-                    $('#popupDialog').popup('open');
-                }, 0); // delay above zero
-            }
-        });
